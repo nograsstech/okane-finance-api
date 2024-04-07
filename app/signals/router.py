2f7df72acfccdb4d4e5ea5a235cd5b735b7818b0
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2024-04-07 17:41:45 +0700

    feat: add basic auth

--- app/signals/router.py
+++ app/signals/router.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from app.signals.dto import SignalRequestDTO, SignalResponseDTO, BacktestResponseDTO
 from app.signals import service
 from starlette.status import HTTP_200_OK
+from app.auth.basic_auth import get_current_username
 
 router = APIRouter(
     prefix="/signals",
@@ -13,16 +14,18 @@ router = APIRouter(
 
 # Query parameters: ticker, start, end, interval, strategy, and parameters
 @router.get("/", response_model=SignalResponseDTO, status_code=HTTP_200_OK)
-async def get_signals(params: SignalRequestDTO = Depends()) -> SignalResponseDTO:
-    
+async def get_signals(
+    params: SignalRequestDTO = Depends(), username: str = Depends(get_current_username)
+) -> SignalResponseDTO:
+
     data = await service.get_signals(
-        ticker = params.ticker,
-        interval = params.interval,
-        period = params.period,
-        strategy = params.strategy,
-        parameters = params.parameters,
-        start = params.start,
-        end = params.end,
+        ticker=params.ticker,
+        interval=params.interval,
+        period=params.period,
+        strategy=params.strategy,
+        parameters=params.parameters,
+        start=params.start,
+        end=params.end,
     )
     return data
 
@@ -30,12 +33,12 @@ async def get_signals(params: SignalRequestDTO = Depends()) -> SignalResponseDTO
 @router.get("/backtest", response_model=BacktestResponseDTO, status_code=HTTP_200_OK)
 async def backtest(params: SignalRequestDTO = Depends()) -> BacktestResponseDTO:
     data = await service.get_backtest_result(
-        ticker = params.ticker,
-        interval = params.interval,
-        period = params.period,
-        strategy = params.strategy,
-        parameters = params.parameters,
-        start = params.start,
-        end = params.end,
+        ticker=params.ticker,
+        interval=params.interval,
+        period=params.period,
+        strategy=params.strategy,
+        parameters=params.parameters,
+        start=params.start,
+        end=params.end,
     )
     return data
