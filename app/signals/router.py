6960da83491ee4df94f6dde05af9835ba2d9524f
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2024-04-12 16:25:53 +0700

    wip: fastapi background_task

--- app/signals/router.py
+++ app/signals/router.py
@@ -1,15 +1,25 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, BackgroundTasks
 from dotenv import load_dotenv
 from app.signals.dto import SignalRequestDTO, SignalResponseDTO, BacktestResponseDTO
 from app.signals import service
 from starlette.status import HTTP_200_OK
 from app.auth.basic_auth import get_current_username
+import uuid
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+executor = ThreadPoolExecutor(max_workers=5)
 
 router = APIRouter(
     prefix="/signals",
     tags=["signals"],
     responses={404: {"description": "Not found"}},
 )
+
+def run_in_executor(func, *args, **kwargs):
+    def wrapper():
+        return func(*args, **kwargs)
+    loop = asyncio.new_event_loop()
+    return asyncio.ensure_future(loop.run_in_executor(executor, wrapper))
 
 
 # Query parameters: ticker, start, end, interval, strategy, and parameters
@@ -30,15 +40,26 @@ async def get_signals(
     return data
 
 
-@router.get("/backtest", response_model=BacktestResponseDTO, status_code=HTTP_200_OK)
-async def backtest(params: SignalRequestDTO = Depends()) -> BacktestResponseDTO:
-    data = await service.get_backtest_result(
-        ticker=params.ticker,
-        interval=params.interval,
-        period=params.period,
-        strategy=params.strategy,
-        parameters=params.parameters,
-        start=params.start,
-        end=params.end,
+@router.get("/backtest", status_code=HTTP_200_OK) #response_model=BacktestResponseDTO
+async def backtest(
+    background_tasks: BackgroundTasks, params: SignalRequestDTO = Depends()
+) : #-> BacktestResponseDTO
+    myuuid = uuid.uuid4()
+    
+    async def long_task():
+        asyncio.run(service.get_backtest_result(
+            ticker=params.ticker,
+            interval=params.interval,
+            period=params.period,
+            strategy=params.strategy,
+            parameters=params.parameters,
+            start=params.start,
+            end=params.end,
+        ))
+
+    background_tasks.add_task(
+        run_in_executor,
+        long_task
     )
-    return data
+    long_task()
+    return myuuid
