459366c8ddf9a777f420666b9bbf7489396cc54e
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2025-01-15 23:30:24 +0700

    feat: add bollinger_rsi strategies

--- app/signals/strategies/calculate.py
+++ app/signals/strategies/calculate.py
@@ -1,6 +1,9 @@
-from .ema_bollinger import ema_bollinger_signals
-from .ema_bollinger_1_low_risk import ema_bollinger_signals as ema_bollinger_signals_low_risk
-from .macd_1 import macd_1
+from .clf_bollinger_rsi.clf_bollinger_rsi_15m import clf_bollinger_signals_15m
+from .clf_bollinger_rsi.clf_bollinger_rsi import clf_bollinger_signals
+from .clf_bollinger_rsi.eurjpy_bollinger_rsi_60m import eurjpy_bollinger_rsi_60m
+from .ema_bollinger.ema_bollinger import ema_bollinger_signals
+from .ema_bollinger_1_low_risk.ema_bollinger_1_low_risk import ema_bollinger_signals as ema_bollinger_signals_low_risk
+from .macd_1.macd_1 import macd_1
 
 def calculate_signals(df, df1d, strategy, parameters):
     print(strategy, parameters)
@@ -11,6 +14,12 @@ def calculate_signals(df, df1d, strategy, parameters):
         return ema_bollinger_signals_low_risk(df, parameters)
       elif strategy == "macd_1":
         return macd_1(df, df1d, parameters)
+      elif strategy == "clf_bollinger_rsi":
+        return clf_bollinger_signals(df, parameters)
+      elif strategy == "clf_bollinger_rsi_15m":
+        return clf_bollinger_signals_15m(df, parameters)
+      elif strategy == "eurjpy_bollinger_rsi_60m":
+        return eurjpy_bollinger_rsi_60m(df, parameters)
       else:
           return None
     except Exception as e:
@@ -27,6 +36,12 @@ async def calculate_signals_async(df, df1d, strategy, parameters):
         return ema_bollinger_signals_low_risk(df, parameters)
   elif strategy == "macd_1":
       return macd_1(df, df1d, parameters)
+  elif strategy == "clf_bollinger_rsi":
+      return clf_bollinger_signals(df, parameters)
+  elif strategy == "clf_bollinger_rsi_15m":
+      return clf_bollinger_signals_15m(df, parameters)
+  elif strategy == "eurjpy_bollinger_rsi_60m":
+      return eurjpy_bollinger_rsi_60m(df, parameters)
   else:
       return None
     
---
+++ app/signals/strategies/clf_bollinger_rsi/eurjpy_bollinger_rsi_60m_backtest.py
@@ -0,0 +1,111 @@
+from backtesting import Strategy
+from backtesting import Backtest
+import multiprocessing as mp
+if mp.get_start_method(allow_none=True) != 'fork':
+    mp.set_start_method('fork', force=True)
+
+def backtest(df, strategy_parameters, size = 0.03, skip_optimization=False, best_params=None):
+    dftest = df[:]
+    
+    # TODO: make these variables parameters instead
+    # trades_actions = None
+    margin = 1/200
+    cash = 100000
+    size = 0.01
+    lot_size = size
+
+    def SIGNAL():
+        return dftest.TotalSignal
+
+    class EURJPYControlTpAndSlSeparately_MultiTrade_60m(Strategy):
+        mysize = 0.02
+        slcoef = 3
+        TPcoef = 2
+        trades_actions = []
+        
+        def init(self):
+            super().init()
+            self.signal1 = self.I(SIGNAL)
+
+        def next(self):
+            try:
+                super().next()
+                slatr = self.slcoef*self.data.atr[-1]
+                tpatr = self.TPcoef*self.data.atr[-1]
+            
+                if self.signal1==2 and len(self.trades)==0:
+                    sl1 = self.data.Close[-1] - slatr
+                    tp1 = self.data.Close[-1] + tpatr
+                    self.buy(sl=sl1, tp=tp1, size=self.mysize)
+                    
+                    if (strategy_parameters['best']):
+                        self.trades_actions.append({
+                            "datetime": self.data.index[-1].strftime('%Y-%m-%d %H:%M:%S.%f'),
+                            "trade_action": "buy",
+                            "entry_price": self.data.Close[-1],
+                            "price": self.data.Close[-1],
+                            "sl": sl1,
+                            "tp": tp1,
+                            "size": self.mysize,
+                        })
+
+                if self.signal1==1 and len(self.trades)==0:
+                    sl1 = self.data.Close[-1] + slatr
+                    tp1 = self.data.Close[-1] - tpatr
+                    self.sell(sl=sl1, tp=tp1, size=self.mysize)
+                    
+                    if (strategy_parameters['best']):
+                        self.trades_actions.append({
+                            "datetime": self.data.index[-1].strftime('%Y-%m-%d %H:%M:%S.%f'),
+                            "trade_action": "sell",
+                            "entry_price": self.data.Close[-1],
+                            "price": self.data.Close[-1],
+                            "sl": sl1,
+                            "tp": tp1,
+                            "size": self.mysize,
+                        })
+            except:
+                pass
+    
+    # Do optimization if skip_optimization is False
+    if (not skip_optimization):
+        print("Optimizing...")
+        bt = Backtest(df, EURJPYControlTpAndSlSeparately_MultiTrade_60m, cash=100000, margin=1/200, commission=0.000) #0.0002
+        stats, heatmap = bt.optimize(slcoef=[i/10 for i in range(3, 101, 5)],
+                    TPcoef=[i/10 for i in range(10, 61, 5)],
+                    maximize='Return [%]', max_tries=500,
+                        random_state=0,
+                        return_heatmap=True)
+        
+        # Convert multiindex series to dataframe
+        heatmap_df = heatmap.unstack()
+        # find the one best parameters from heatmap_df
+        best_params = heatmap_df.idxmax()
+
+        # Find the maximum value over the entire DataFrame
+        max_value = heatmap_df.max().max()
+
+        # Find the index of the maximum value
+        best_params = (heatmap_df == max_value).stack().idxmax()
+
+        print(best_params)
+    else:
+        print("Optimization is skipped and best params provided", best_params)
+        
+    strategy_parameters = {
+        "best": True,
+        "TPcoef": best_params[0],
+        "slcoef": best_params[1],
+        "tpslRatio": best_params[0] / best_params[1]
+    }
+    
+    print(strategy_parameters)
+    
+    EURJPYControlTpAndSlSeparately_MultiTrade_60m.slcoef = strategy_parameters["slcoef"]
+    EURJPYControlTpAndSlSeparately_MultiTrade_60m.TPcoef = strategy_parameters["TPcoef"]
+    
+    bt_best = Backtest(dftest, EURJPYControlTpAndSlSeparately_MultiTrade_60m, cash=cash, margin=margin)
+    stats = bt_best.run()
+    trades_actions = bt_best._strategy.trades_actions
+    
+    return bt_best, stats, trades_actions, strategy_parameters
