cb63ac6545d4e301e293242e23711c9a66fe4996
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2025-02-19 03:12:07 +0700

    feat: setup langgraph

--- app/main.py
+++ app/main.py
@@ -3,11 +3,13 @@ from app.base.interface import RootResponse
 from app.news.router import router as newsRouter
 from app.ticker.router import router as tickerRouter
 from app.signals.router import router as signalsRouter
+from app.ai.router import router as aiRouter
 
 app = FastAPI()
 app.include_router(newsRouter)
 app.include_router(tickerRouter)
 app.include_router(signalsRouter)
+app.include_router(aiRouter)
 
 @app.get("/", response_model=RootResponse)
 def read_root():
