95f52869a41003ddd4d3e49e7073d37e2db0ef25
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2025-04-25 22:31:23 +0700

    feat: add chainlit app

---
+++ app/chainlit/chainlit.py
@@ -0,0 +1,93 @@
+from operator import itemgetter
+from typing import Dict, Optional
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import Runnable, RunnablePassthrough, RunnableLambda
+from langchain.schema.runnable.config import RunnableConfig
+from langchain.memory import ConversationBufferMemory
+from chainlit.types import ThreadDict
+import chainlit as cl
+
+
+def setup_runnable():
+    memory = cl.user_session.get("memory")
+    model = ChatGoogleGenerativeAI(
+        model = "gemini-2.0-flash",
+        streaming=True)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful chatbot"),
+            MessagesPlaceholder(variable_name="history"),
+            ("human", "{question}"),
+        ]
+    )
+
+    runnable = (
+        RunnablePassthrough.assign(
+            history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+        )
+        | prompt
+        | model
+        | StrOutputParser()
+    )
+    cl.user_session.set("runnable", runnable)
+
+
+@cl.password_auth_callback
+def auth():
+    return cl.User(identifier="test")
+  
+@cl.oauth_callback
+def oauth_callback(
+  provider_id: str,
+  token: str,
+  raw_user_data: Dict[str, str],
+  default_user: cl.User,
+) -> Optional[cl.User]:
+  return default_user
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    cl.user_session.set("memory", ConversationBufferMemory(return_messages=True))
+    setup_runnable()
+
+
+@cl.on_chat_resume
+async def on_chat_resume(thread: ThreadDict):
+    memory = ConversationBufferMemory(return_messages=True)
+    root_messages = [m for m in thread["steps"] if m["parentId"] is None]
+    for message in root_messages:
+        if message["type"] == "user_message":
+            memory.chat_memory.add_user_message(message["output"])
+        else:
+            memory.chat_memory.add_ai_message(message["output"])
+
+    cl.user_session.set("memory", memory)
+
+    setup_runnable()
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    memory = cl.user_session.get("memory")
+
+    runnable = cl.user_session.get("runnable")
+
+    if not message.content.strip():
+        await message.reply("Please provide a valid question.")
+        return
+
+    res = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+        {"question": message.content}, 
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await res.stream_token(chunk)
+
+    await res.send()
+
+    memory.chat_memory.add_user_message(message.content)
+    memory.chat_memory.add_ai_message(res.content)
